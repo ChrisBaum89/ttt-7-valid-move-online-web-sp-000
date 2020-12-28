779844e0ed898316a0a769b18a26b346a67e6cde
Chris Baum <christopher.baum@valpo.edu>
2020-12-28 17:45:50 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,10 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if index >= 0 && index <= 8
-    if board[index] == " " || board[index] == "" || board[index] == nil
-      false
-    else board[index] == "X" || board[index] == "O"
-      true
-    end
+  if index >= 0 && index <= 8 && board[index] == " " || board[index] == "" || board[index] == nil
+    true
   end
 end
